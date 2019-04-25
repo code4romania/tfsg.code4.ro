d8829f8edc053475537f0cff057f1b530aeb0334
Andrei Ioniță <hi@andrei.io>
2019-04-25 17:18:27 +0300

    Migrate build process to webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-	'root': true,
-	'extends': 'eslint:recommended',
-	'env': {
-		'browser': true,
-		'node': true,
+	root: true,
+	extends: 'eslint:recommended',
+	env: {
+		browser: true,
+		node: true,
+		es6: true
 	},
-	'globals': {
-		'Modernizr': true,
+	globals: {
+		Modernizr: true,
 	},
-	'rules': {
+	rules: {
 		'no-cond-assign': 'off',
 		'no-console': 'off',
 	},
