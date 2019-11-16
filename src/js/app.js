615b2873b5cd140c483ca37872776efdb7482896
Alex Stefanescu <alexandra.stefanescu@code4.ro>
2019-11-16 23:10:10 +0200

    Add sponsors and partners per city

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 let countdown = require('./countdown'),
 	tests = require('./feature-tests'),
 	navbar = require('./navbar'),
-	form = require('./form');
+	form = require('./form'),
+	partners = require('./partners');
 
 tests();
 countdown();
 navbar();
 form();
+partners();
